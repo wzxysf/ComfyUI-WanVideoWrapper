644f9aeaa3da436a78b063707ef88c523557190b
kijai <40791699+kijai@users.noreply.github.com>
2025-08-21 22:14:37 +0300

    Fix in multitalk sampling

--- nodes.py
+++ nodes.py
@@ -2033,6 +2033,7 @@ class WanVideoSampler:
             context = get_context_scheduler(context_schedule)
 
         # vid2vid
+        noise_mask=None
         if samples is not None and not multitalk_sampling:
             saved_generator_state = samples.get("generator_state", None)
             if saved_generator_state is not None:
