10d3a0fbef2dd2865e6aa0fbf64201bf484ea18b
kijai <40791699+kijai@users.noreply.github.com>
2025-08-30 23:34:14 +0300

    Update model.py

--- wanvideo/modules/model.py
+++ wanvideo/modules/model.py
@@ -2520,7 +2520,7 @@ class WanModel(torch.nn.Module):
                     self.controlnet.to(self.offload_device)
 
             # Asynchronous block offloading with CUDA streams and events
-            cuda_stream = torch.cuda.Stream(device=device, priority=0)
+            cuda_stream = None #torch.cuda.Stream(device=device, priority=0)
             events = [torch.cuda.Event() for _ in self.blocks]
             swap_start_idx = len(self.blocks) - self.blocks_to_swap if self.blocks_to_swap > 0 else len(self.blocks)
 
